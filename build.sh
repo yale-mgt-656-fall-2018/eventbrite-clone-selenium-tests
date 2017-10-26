70bd1a88a6c148dce074abf2310223e9a71d3740
Kyle L. Jensen <kljensen@gmail.com>
2017-10-26 07:28:34 -0500

    Update build script and start tracking release binaries

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-GOOS=windows GOARCH=386 go build -o ./eventbrite-clone-selenium-tests.exe
-mv ./eventbrite-clone-selenium-tests.exe tmp/windows
-GOOS=linux GOARCH=386 go build -o ./eventbrite-clone-selenium-tests
-mv ./eventbrite-clone-selenium-tests tmp/linux
-go build -o eventbrite-clone-selenium-tests
-mv ./eventbrite-clone-selenium-tests tmp/mac
+git tag $1
+
+GOOS=windows GOARCH=386 go build -o ./eventbrite-clone-selenium-tests-windows-$1.exe
+mv ./eventbrite-clone-selenium-tests-windows-$1.exe releases
+
+GOOS=linux GOARCH=386 go build -o ./eventbrite-clone-selenium-tests-linux-$1
+mv ./eventbrite-clone-selenium-tests-linux-$1 releases
+
+go build -o eventbrite-clone-selenium-tests-mac-$1
+mv ./eventbrite-clone-selenium-tests-mac-$1 releases
