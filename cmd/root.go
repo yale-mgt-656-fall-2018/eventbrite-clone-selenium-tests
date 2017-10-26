35253dea2870c33ddec6fefe731b17e702a8345d
Kyle L. Jensen <kljensen@gmail.com>
2017-10-26 07:25:32 -0500

    Update root command documentation

--- cmd/root.go
+++ cmd/root.go
@@ -11,14 +11,14 @@ var cfgFile string
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-	Use:   "foo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "eventbrite-clone-selenium-tests-$ARCH-$VERSION",
+	Short: "Runs tests and does grading for mgt656/mgt660 class project",
+	// 	Long: `A longer description that spans multiple lines and likely contains
+	// examples and usage of using your application. For example:
+	//
+	// Cobra is a CLI library for Go that empowers applications.
+	// This application is a tool to generate the needed files
+	// to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
