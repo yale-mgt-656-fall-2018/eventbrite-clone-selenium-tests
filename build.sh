d69420b6385a95d9cdd618599c2886bb76ed6e0a
Kyle L. Jensen <kljensen@gmail.com>
2018-10-25 11:28:57 -0400

    Update build process

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-git tag $1
+#git tag $1
 
 GOOS=windows GOARCH=386 go build -o ./eventbrite-clone-selenium-tests-windows-$1.exe
 mv ./eventbrite-clone-selenium-tests-windows-$1.exe releases
@@ -8,3 +8,5 @@ mv ./eventbrite-clone-selenium-tests-linux-$1 releases
 
 go build -o eventbrite-clone-selenium-tests-mac-$1
 mv ./eventbrite-clone-selenium-tests-mac-$1 releases
+
+#aws s3 sync --acl public-read releases s3://files.656.mba --exclude=".git"
